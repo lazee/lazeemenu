4627b564c672751501bfe269f51de45d06faac7a
lazee <jakob.nielsen@amedia.no>
2016-03-16 00:21:48 +0100

    Preparing for first release: Fixed js, examples and polished css and themes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
 var del = require('del');
 var runSequence = require('run-sequence');
 
@@ -16,7 +20,33 @@ gulp.task('deepclean', function() {
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('minify-css', function () {
+	gulp.src('./dist/*.css')
+		.pipe(cssmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('./dist'));
+});
+
+gulp.task('copy', function () {
+    gulp.src(['./LICENSE']).pipe(gulp.dest('./dist'));
+    gulp.src(['./README.md']).pipe(gulp.dest('./dist'));
+    gulp.src(['./src/*.js']).pipe(gulp.dest('./dist'));
+});
+
+gulp.task('compress', function() {
+  return gulp.src('./src/*.js')
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./src/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('cool-reporter'));
 });
 
 gulp.task('sass:watch', function () {
@@ -24,5 +54,5 @@ gulp.task('sass:watch', function () {
 });
 
 gulp.task('default', function(callback) {
-  runSequence('clean', 'sass');
+  runSequence('clean', 'sass', 'minify-css', 'lint', 'copy', 'compress');
 });
