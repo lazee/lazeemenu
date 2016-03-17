4866aca506922502bfd9c4bc86929bec9bdfaae3
lazee <jakob.nielsen@amedia.no>
2016-03-17 23:58:19 +0100

    Major cleanup of SASS and JS. Also started to add README and release stuff with Gulp

--- README.md
+++ README.md
@@ -1,3 +1,101 @@
 # LazeeMenu
 
+LazeeMenu is a Javascript plugin that turns a nested unordered list of links into a vertical multi-level
+navigation menu. It is heavily inspired by the similar menu used by Github on its reference pages, but with support for more levels. It also support not having links on the expandable menu items, making it very flexible. At the moment LazeeMenu is implemented as a JQuery plugin, but a pure Javascript version will be released soon.
 
+## Getting started
+
+Download the plugin, unzip it and copy the files to your project folder. Then include the css and js inside your HTML.
+
+```HTML
+<head>
+    <!-- Load LazeeMenu CSS -->
+    <script type="text/javascript" src="/lazeemenu/lazeemenu.css"></script>
+</head>
+<body>
+    <!-- Add this at the bottom of the body tag -->
+    <!-- Load JQuery -->
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <!-- Load LazeeMenu -->
+    <script type="text/javascript" src="/lazeemenu/lazeemenu-jquery.js"></script>
+</body>
+```
+
+Sample menu and how to activate:
+
+```HTML
+<ul class="menu-1">
+    <li>
+    <li>
+        <h3><span>Tennis</span></h3>
+        <ul>
+            <li><a href="/">Serves</a></li>
+            <li class="active"><a href="/">Player results</a></li>
+            <li><a href="/">Leagues</a></li>
+            <li><a href="/">Tournaments</a></li>
+        </ul>
+    </li>
+    <li>
+        <h3><span>Ballet</span></h3>
+        <ul>
+            <li><a href="/">Dancers</a></li>
+            <li><a href="/">Famous acts</a></li>
+        </ul>
+    </li>
+</ul>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.menu-1').lazeemenu();
+    });
+</script>
+```
+
+There are also a few options that can be extended:
+
+```HTML
+<script type="text/javascript">
+    $(document).ready(function() {
+        $('.menu-1').lazeemenu({
+            activeClass: 'active',
+            initialState: 'default'        
+        });
+    });
+</script>
+```
+
+## Options
+
+These are the options you can set during activation of a menu.
+
+### activeClass
+
+  * **Type:** `string`
+  * **Default:** `active`
+
+The name of the class name used in *li* elements to indicate what link that should be marked as active.
+
+### initialState
+
+  * **Type:** `string`
+  * **Default:** `default`
+
+You can set this value to `expanded` if you initially want the entire menu expanded. Setting it to `collapsed` will collapse the menu. Finally `default` (Which is the default ;)) will make sure that the active link is expanded. Everything else will be collapsed.
+
+
+## Public methods
+
+### expandAll
+
+Expand all menu items
+
+```js
+$(selector).lazeemenu('expandAll');
+```
+
+### expandAll
+
+Collapse all menu items
+
+```js
+$(selector).lazeemenu('collapseAll');
+```
--- gulpfile.js
+++ gulpfile.js
@@ -6,53 +6,92 @@ var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var nunjucksRender = require('gulp-nunjucks-render');
+var ghPages = require('gulp-gh-pages');
+var markdown = require('gulp-markdown');
+var bump = require('gulp-bump');
 var del = require('del');
 var runSequence = require('run-sequence');
 
 gulp.task('clean', function() {
-  return del(['dist']);
+    return del(['dist']);
 });
 
 gulp.task('deepclean', function() {
-  return del(['dist', 'node_modules']);
+    return del(['dist', 'node_modules']);
 });
 
-gulp.task('sass', function () {
-  return gulp.src('./src/sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
+gulp.task('sass', function() {
+    return gulp.src('./src/sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/package'));
 });
 
-gulp.task('minify-css', function () {
-	gulp.src('./dist/*.css')
-		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('./dist'));
+gulp.task('minify-css', function() {
+    gulp.src('./dist/package/*.css')
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist/package'));
 });
 
-gulp.task('copy', function () {
-    gulp.src(['./LICENSE']).pipe(gulp.dest('./dist'));
-    gulp.src(['./README.md']).pipe(gulp.dest('./dist'));
-    gulp.src(['./src/*.js']).pipe(gulp.dest('./dist'));
+gulp.task('copy', function() {
+    gulp.src(['./LICENSE']).pipe(gulp.dest('./dist/package'));
+    gulp.src(['./README.md']).pipe(gulp.dest('./dist/package'));
+    gulp.src(['./src/*.js']).pipe(gulp.dest('./dist/package'));
 });
 
 gulp.task('compress', function() {
-  return gulp.src('./src/*.js')
-    .pipe(uglify())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./dist'));
+    return gulp.src('./src/*.js')
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./dist/package'));
 });
 
 gulp.task('lint', function() {
-  return gulp.src('./src/*.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('cool-reporter'));
+    return gulp.src('./src/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('cool-reporter'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./src/sass/**/*.scss', ['sass']);
+gulp.task('sass:watch', function() {
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('pages', function() {
+    gulp.src(['./site/stylesheets/*.css']).pipe(gulp.dest('./dist/pages/stylesheets'));
+    gulp.src(['./site/javascripts/*.js']).pipe(gulp.dest('./dist/pages/javascripts'));
+    gulp.src(['./dist/package/*.css']).pipe(gulp.dest('./dist/pages/stylesheets'));
+    gulp.src(['./dist/package/*.js']).pipe(gulp.dest('./dist/pages/javascripts'));
+    gulp.src(['./node_modules/jquery/dist/jquery.min.js']).pipe(gulp.dest('./dist/pages/javascripts'));
+    return gulp.src('site/pages/*.html')
+        .pipe(nunjucksRender({
+            path: ['site/templates/', 'dist/md/']
+        }))
+        .pipe(gulp.dest('dist/pages'));
+});
+
+gulp.task('pages-deploy', function() {
+  return gulp.src('./dist/pages/**/*')
+    .pipe(ghPages());
+});
+
+
+gulp.task('md', function () {
+    return gulp.src('README.md')
+        .pipe(markdown())
+        .pipe(gulp.dest('dist/md/'));
+});
+
+gulp.task('bump', function(){
+  gulp.src(['./bower.json', './package.json'])
+  .pipe(bump({type:'patch'}))
+  .pipe(gulp.dest('./'));
 });
 
 gulp.task('default', function(callback) {
-  runSequence('clean', 'sass', 'minify-css', 'lint', 'copy', 'compress');
+    runSequence('clean', 'sass', 'minify-css', 'lint', 'copy', 'compress', 'md', 'pages');
 });
